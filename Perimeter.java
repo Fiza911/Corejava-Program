0eb8dfa62fe81cc47cd4e3d2be3771a7ba4b8881
Fiza911 <122105081+Fiza911@users.noreply.github.com>
2025-01-13 21:19:40 +0530

    Add files via upload

--- Perimeter.java
+++ Perimeter.java
@@ -1,25 +1,24 @@
 //write a program to find the perimeter of the square
 package Corejava;
 import java.util.Scanner;
-public class Perimeter {
-	    public static void main(String[] args) {
+public class Perimeter 
+{
+	    public static void main(String[] args) //this is the main method
+	    {
 	        // Create Scanner object to take input from user
 	        Scanner sc= new Scanner(System.in);
-
 	        // Declare a variable to store the side length of the square
 	        double side;
-
 	        // Take input for the side length of the square
 	        System.out.print("Enter the length of the square: ");
 	        side = sc.nextDouble();
-
 	        // Calculate the perimeter of the square
-	        double perimeter = 4 * side;
-
+	        double perimeter =4*side;
 	        // Display the perimeter
 	        System.out.println("The perimeter of the square is: " + perimeter);
-
 	    }
-	
 }
 
+/*Output
+ Enter the length of the square: 5
+The perimeter of the square is: 20.0*/
