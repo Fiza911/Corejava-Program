a48ad09b5dd5f2f16ddd735238b673fa37eb5159
Fiza911 <122105081+Fiza911@users.noreply.github.com>
2025-01-23 21:54:11 +0530

    Add files via upload

---
+++ ThisMethod.java
@@ -0,0 +1,25 @@
+package Corejava;
+public class ThisMethod 
+{
+	   // Method to display a simple message
+	    void display()
+	    {
+	    	 System.out.println("I am display method");	  
+	    }
+	    // Method that calls the `display` method using the this keyword
+	    void show()
+	    {
+	    	// Calls the `display` method of the current object
+	    	this.display();
+	    	System.out.println("I am show method");
+	    }
+		public static void main(String[] args)
+		{
+			ThisMethod obj=new ThisMethod();
+			obj.show();
+		}
+}
+
+/*Output:
+I am display method
+I am show method*/
